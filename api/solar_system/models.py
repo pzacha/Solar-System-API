a1cf9d0c1d3f3a2fae2444f8e76b97b5bd4b6c98
Piotr Zacha <pzacha11@gmail.com>
2020-03-09 09:34:08 +0100

    Changed Planet model

--- api/solar_system/models.py
+++ api/solar_system/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Planet(models.Model):
     name = models.CharField(max_length=30)
-    period = models.IntegerField()
-    diameter = models.IntegerField()
-    mass = models.IntegerField()
-    have_satellites = models.BooleanField()
+    period = models.FloatField()                            # Period of a planet in years.
+    diameter = models.IntegerField()                        # Diameter of a planet in kilometers.
+    mass = models.FloatField()                              # Mass of a planet in Earth masses.
+    have_satellites = models.BooleanField(default=False)    # Boolean value describing if planet has natural satellites.
+
+    def __str__(self):
+        return self.name
